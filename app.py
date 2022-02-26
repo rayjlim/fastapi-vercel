c22be6dca5fd76abe9dfc2a02324136dcd7595de
ray <rayjlim1@gmail.com>
2022-02-25 20:41:21 -0800

    feat: ✨ endpoint to get random quote

--- app.py
+++ app.py
@@ -2,14 +2,24 @@ from datetime import datetime
 from typing import Optional
 import httpx
 from fastapi import FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import models
+import json
+import random
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request, username: str = None):
@@ -21,6 +31,37 @@ def index(request: Request, username: str = None):
     context = {"request": request, "user": user}
 
     return templates.TemplateResponse("index.html", context=context)
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/quotes/")
+async def get_quote():
+    text_file = open("quotes.json", "r")
+    data = json.load(text_file)
+    
+    # data = text_file.read()    
+    text_file.close()
+    
+    return data
+
+class Message:
+  def __init__(self, message, author):
+    self.message = message
+    self.author = author
+
+def mapMessage(item):
+    return Message(item['quote'], item['author'])
+
+@app.get("/quote/")
+async def get_quote():
+    text_file = open("quotes.json", "r")
+    data = json.load(text_file)
+    text_file.close()
+    result = list(map(mapMessage, data['quotes']))
+    # return data
+    return random.choice(result)
 
 
 @app.get("/{username}", response_model=models.GithubUserModel)
@@ -39,3 +80,4 @@ def get_github_profile(request: Request, username: str) -> Optional[models.Githu
     user.created_at = datetime.strptime(user.created_at, "%Y-%m-%dT%H:%M:%SZ").strftime("%d/%m/%y")
 
     return user
+
